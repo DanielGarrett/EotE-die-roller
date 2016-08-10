6d3fdf9980f3a5b76177cc1ae9cd2f66eb5671f6
Daniel Garrett <daniel.j.garrett@gmail.com>
2016-08-09 20:45:40 -0500

    create sample tests

---
+++ tests/dieRollerTests.js
@@ -0,0 +1,6 @@
+QUnit.test( "hello test", function( assert ) {
+  assert.ok( 1 == "1", "Passed!" );
+});
+QUnit.test( "hello test2", function( assert ) {
+  assert.notOk( 1 === "1", "Passed!" );
+});
