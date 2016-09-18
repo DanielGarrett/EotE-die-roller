8c4dc332258f3fb860ed5a42e70a82edd4d832bc
Daniel Garrett <daniel.j.garrett@gmail.com>
2016-09-17 21:57:55 -0500

    add dice pool

--- src/diceSelectionViewModel.js
+++ src/diceSelectionViewModel.js
@@ -6,7 +6,43 @@ var diceSelectionViewModel = function() {
     self.DifficultyDiceCount = ko.observable(0);
     self.ChallengeDiceCount = ko.observable(0);
     self.SetbackDiceCount = ko.observable(0);
+    
+    var abilityDie = new diceViewModel('http://game2.ca/eote/abilitybg.png', rollAbilityDie);
+    var proficiencyDie = new diceViewModel('http://game2.ca/eote/proficiencybg.png', rollProficiencyDie);
+    var boostDie = new diceViewModel('http://game2.ca/eote/boostbg.png', rollBoostDie);
+    var difficultyDie = new diceViewModel('http://game2.ca/eote/difficultybg.png', rollDifficultyDie);
+    var challengeDie = new diceViewModel('http://game2.ca/eote/challengebg.png', rollChallengeDie);
+    var setbackDie = new diceViewModel('http://game2.ca/eote/setbackbg.png', rollSetbackDie);
 
+    self.DicePool = ko.computed(function() {
+        var diceArray = [];
+        for(var x = 0; x < self.AbilityDiceCount(); x++){
+            diceArray.push(abilityDie);
+        }
+        for(var x = 0; x < self.ProficiencyDiceCount(); x++){
+            diceArray.push(proficiencyDie);
+        }
+        for(var x = 0; x < self.BoostDiceCount(); x++){
+            diceArray.push(boostDie);
+        }
+        for(var x = 0; x < self.DifficultyDiceCount(); x++){
+            diceArray.push(difficultyDie);
+        }
+        for(var x = 0; x < self.ChallengeDiceCount(); x++){
+            diceArray.push(challengeDie);
+        }
+        for(var x = 0; x < self.SetbackDiceCount(); x++){
+            diceArray.push(setbackDie);
+        }
+        return diceArray;
+    });
+
+};
+
+var diceViewModel = function(url, rollFunction) {
+    var self = this;
+    self.ImageUrl = url;
+    self.Roll = rollFunction;
 };
 
 (function(){
